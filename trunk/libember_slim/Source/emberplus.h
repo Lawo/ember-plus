a9a7fce16131bf2e1d120db8fdb8efdce21a2be0
pbo@l-s-b.de <pbo@l-s-b.de@de6b03e0-4000-86bf-52c6-5a79c10581ff>
2014-04-23 11:58:01 +0000

    libember_slim: corrected version number

--- trunk/libember_slim/Source/emberplus.h
+++ trunk/libember_slim/Source/emberplus.h
@@ -25,7 +25,7 @@
   * The upper byte is the major version number.
   * The lower byte is the minor version number.
   */
-#define EMBER_LIBRARY_VERSION (0x0133) //1.51
+#define EMBER_LIBRARY_VERSION (0x013E) //1.62
 
 #include "glowtx.h"
 #include "glowrx.h"

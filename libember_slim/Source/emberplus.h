773356a5e9a7216ed2da14b7edf2de2b362196a1
Kimon.Hoffmann <Kimon.Hoffmann@lawo.com>
2014-06-10 11:39:21 +0000

    Changed version numbers to 1.6.3 indicating the post-1.6.2 development version the branch currently represents.

--- libember_slim/Source/emberplus.h
+++ libember_slim/Source/emberplus.h
@@ -25,7 +25,7 @@
   * The upper byte is the major version number.
   * The lower byte is the minor version number.
   */
-#define EMBER_LIBRARY_VERSION (0x0132) //1.50
+#define EMBER_LIBRARY_VERSION (0x013F) //1.63
 
 #include "glowtx.h"
 #include "glowrx.h"

c4bfa2743b905990dcd202b6f42a8e1bbca0c0b7
Kimon Hoffmann <Kimon.Hoffmann@lawo.com>
2016-07-19 16:52:32 +0200

    Fixed a syntax error introduced by automatically editing the licensing statement.

--- libember/Headers/ember/glow/util/traits/ValueConverterTraits.hpp
+++ libember/Headers/ember/glow/util/traits/ValueConverterTraits.hpp
@@ -1,9 +1,14 @@
-#ifndef __LIBEMBER_GLOW_UTIL_TRAITS_VALUECONVERTERTRAITS_HPP
-#define __LIBEMBER_GLOW_UTIL_TRAITS_VALUECONVERTERTRAITS_HPP
+/*
+    libember -- C++ 03 implementation of the Ember+ Protocol
 
     Copyright (C) 2012-2016 Lawo GmbH (http://www.lawo.com).
     Distributed under the Boost Software License, Version 1.0.
     (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+*/
+
+#ifndef __LIBEMBER_GLOW_UTIL_TRAITS_VALUECONVERTERTRAITS_HPP
+#define __LIBEMBER_GLOW_UTIL_TRAITS_VALUECONVERTERTRAITS_HPP
+
     /**
      * Default implementation for a value converter.
      */
